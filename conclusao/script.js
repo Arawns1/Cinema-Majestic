a0a507984c5e3cc0166689eacfab0fe567f4b076
boubeejul <juliana.boubee@gmail.com>
2023-05-06 13:08:40 -0300

    Geração de ingresso na tela de conclusão

--- conclusao/script.js
+++ conclusao/script.js
@@ -1,3 +1,7 @@
 var btnVoltarMenu = document.querySelector("#voltarMenu").addEventListener("click", e => {
   window.location.href = "../home/index.html"
 })
+
+var gerarIngresso = document.querySelector("#imprimirIngresso").addEventListener("click", e => {
+    window.print()
+})
